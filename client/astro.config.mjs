dddd5bb30c4cbd23c4d91b64f89b12ca5bc955eb
not201 <not201@proton.me>
2025-11-29 13:50:53 -0500

    Sidebar implement

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -3,9 +3,18 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  i18n: {
+    locales: ["es", "en"],
+    defaultLocale: "es",
+  },
+
+  integrations: [react()]
 });
